40af08518e70ccdfc1766cb127c48caa059fb301
rrivas <rrivas@utec.edu.pe>
2024-11-08 19:34:47 -0500

    update binary search

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <random>
 #include <iterator>
 #include <thread>
+#include <mutex>
 #include <type_traits>
 
 // Tomado de CPP Reference
-
 void print (const auto rem, const auto& v) {
     std::cout << rem;
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
@@ -110,14 +110,99 @@ void ejemplo_merge_sort() {
 template <
         typename Iterator,
         typename T = typename std::iterator_traits<Iterator>::value_type>
-Iterator binary_search(Iterator left, Iterator right, T value) {
-    if (std::distance(left, right) <= 1) return right;
+Iterator binary_search_utec(Iterator left, Iterator right, T value, Iterator invalid_it = Iterator{}) {
+    if (invalid_it == Iterator{}) invalid_it = right;
+    if (std::distance(left, right) <= 0) return invalid_it;
     auto mid = std::next(left, std::distance(left, right) / 2);
     if (*mid == value) return mid;
     if (*mid > value)
-        return binary_search(left, std::prev(mid));
+        return binary_search_utec(left, std::prev(mid), value, invalid_it);
     else
-        return binary_search(std::next(mid), right);
+        return binary_search_utec(std::next(mid), right, value, invalid_it);
+}
+
+std::mutex mtx;
+
+template <
+        typename Iterator,
+        typename T = typename std::iterator_traits<Iterator>::value_type>
+void binary_search_npar(Iterator left, Iterator right, T value, Iterator& result, Iterator invalid_it = Iterator{}) {
+    if (std::distance(left, right) <= 0) return;
+    auto mid = std::next(left, std::distance(left, right) / 2);
+
+    if (result != invalid_it) {
+        return;
+    }
+    if (*mid == value) {
+        std::unique_lock ul(mtx);
+        result = mid;
+        return;
+    }
+    if (*mid > value)
+        binary_search_npar(left, std::prev(mid), value, result, invalid_it);
+    else
+        binary_search_npar(std::next(mid), right, value, result, invalid_it);
+}
+
+template <
+        typename Iterator,
+        typename T = typename std::iterator_traits<Iterator>::value_type>
+Iterator binary_search_par(Iterator left, Iterator right, T value) {
+
+    // Parametros de los hilos
+    auto nh = std::thread::hardware_concurrency();
+    auto span = nh / std::distance(left, right);
+    auto last_span = nh % std::distance(left, right);
+
+    // iterador compartido
+    auto result_it = right;
+
+    // vector de hilos
+    std::vector<std::thread> vh;
+
+    // fork
+    for (int i = 0; i < nh; ++i) {
+        vh.emplace_back(binary_search_npar<Iterator>,
+                        std::next(left, span*i), std::next(left, span*(i+1)),
+                        value,
+                        std::ref(result_it), right);
+    }
+    // join
+    for (int i = 0; i < nh; ++i) {
+        vh[i].join();
+    }
+    if (result_it == right && last_span > 0) {
+        binary_search_npar(std::next(left, span*nh), right, value, result_it, right);
+    }
+    return result_it;
+}
+
+
+void ejemplo_busqueda_binaria() {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<> dis(1, 50);
+    std::vector<int> v1(30);
+    std::generate(v1.begin(), v1.end(), [&mt, &dis] { return dis(mt); });
+    merge_sort_par(v1.begin(), v1.end());
+    print("v1: ", v1);
+    auto it = binary_search_utec(v1.begin(), v1.end(), 32);
+    if (it != v1.end()) {
+        std::cout << std::distance(v1.begin(), it) << std::endl;
+        std::cout << *it << std::endl;
+    }
+    else {
+        std::cout << "No found" << std::endl;
+    }
+
+    auto it2 = binary_search_par(v1.begin(), v1.end(), 32);
+    if (it2 != v1.end()) {
+        std::cout << std::distance(v1.begin(), it2) << std::endl;
+        std::cout << *it2 << std::endl;
+    }
+    else {
+        std::cout << "No found" << std::endl;
+    }
 }
 
 
@@ -126,6 +211,7 @@ Iterator binary_search(Iterator left, Iterator right, T value) {
 int main() {
 //    ejemplo_uso_merge_2_vectores();
 //    ejemplo_uso_merge_1_vector();
-    ejemplo_merge_sort();
+//    ejemplo_merge_sort();
+    ejemplo_busqueda_binaria();
     return 0;
 }
